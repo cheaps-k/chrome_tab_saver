ee778abe46146170b424025de0d47d0f231f34ac
cheaps-k <cheaps.kk@gmail.com>
2019-10-20 16:59:56 +0900

    AddFunction: 既存タブマークへの保存機能を追加

--- project/js/popup.js
+++ project/js/popup.js
@@ -1,13 +1,60 @@
 window.onload = function() {
-    document.getElementById('restore_btn_open_tabs').addEventListener('click', open_tab_list);
+    document.getElementById('btn_restore_open_tabs').addEventListener('click', open_tab_list);
+    document.getElementById('btn_save_tabs').addEventListener('click', add_tab_list);
 
+    update_save_tab_list();
+    update_save_target_tabmark_list();
     update_restore_tabmark_list();
 };
+
+/* ===== Save functions ===== */
+function add_tab_list() {
+    var tabmark_list = get_tabmark_list();
+    var options_tab_list = document.getElementById('sb_save_tab_list').options;
+    var save_tab_ids = [];
+    for( var i = 0; i < options_tab_list.length; i++ ) {
+        if( options_tab_list[i].selected ) {
+            save_tab_ids.push( options_tab_list[i].value );
+        }
+    }
+    
+    chrome.tabs.query({currentWindow: true}, function( tabs ) {
+        var save_target_id = document.getElementById("sb_save_target_tabmark_list").value;
+        
+        for( var index = 0; index < tabs.length; index++ ) {
+            for( var save_tab_index = 0; save_tab_index < save_tab_ids.length; save_tab_index++ ) {
+                if( tabs[index].id == save_tab_ids[save_tab_index] ) {
+                    tabmark_list[save_target_id].data.push({ name: tabs[index].title, url: tabs[index].url });
+                    chrome.tabs.remove(tabs[index].id);
+                    break;
+                }
+            }
+        }
+        save_tabmark_list( tabmark_list );
+    });
+}
+
+function update_save_tab_list() {
+    chrome.tabs.query({currentWindow: true}, function( tabs ) {
+        var select = document.getElementById("sb_save_tab_list");
+        
+        for( var index = 0; index < tabs.length; index++ ) {
+            var option = document.createElement("option");
+            option.text = tabs[index].title;
+            option.value = tabs[index].id;
+            select.appendChild(option);
+        }
+    });
+}
+
+function update_save_target_tabmark_list() {
+    set_tabmark_list_for_select_box("sb_save_target_tabmark_list");
+}
 
 /* ===== Restore functions ===== */
 function open_tab_list() {
     var tabmark_list = get_tabmark_list();
-    var options = document.getElementById('restore_sb_tabmark_list').options;
+    var options = document.getElementById('sb_restore_tabmark_list').options;
     
     for( var i = 0; i < options.length; i++ ) {
         if( options[i].selected ) {
@@ -26,8 +73,13 @@ function open_tab_list() {
 }
 
 function update_restore_tabmark_list() {
+    set_tabmark_list_for_select_box("sb_restore_tabmark_list");
+}
+
+/* ===== Common functions ===== */
+function set_tabmark_list_for_select_box( select_box_id ) {
     var tabmark_list = get_tabmark_list();
-    var select = document.getElementById("restore_sb_tabmark_list");
+    var select = document.getElementById(select_box_id);
     
     for( id in tabmark_list ) {
         var option = document.createElement("option");
@@ -37,7 +89,6 @@ function update_restore_tabmark_list() {
     }
 }
 
-/* ===== Common functions ===== */
 function get_tabmark_list() {
     try {
         return JSON.parse( localStorage["tabmark"] );
